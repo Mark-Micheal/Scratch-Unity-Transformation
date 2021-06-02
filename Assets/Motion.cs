8114f52919b755d17a379506dd9d2d24ff0c46d6
Mark-Micheal <markmichealhp@outlook.com>
2021-06-02 23:08:50 +0200

    motion blocks parser

--- Assets/Motion.cs
+++ Assets/Motion.cs
@@ -15,32 +15,36 @@ public class Motion : MonoBehaviour
     private float positionIncrement;
     public TextMeshProUGUI say;
     public int x;
+    public float factor = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
        // textFile = "/buildTest.txt";
-        textFile = "E:/unity/VP3/Assets/test.txt";
+        textFile = "Assets/test.txt";
         string[] lines = File.ReadAllLines(textFile);
-        foreach (string line in lines)
+        Parser(lines);
+
+        /*foreach (string line in lines)
         {
             print(line);
             CheckKeyword(line);
 
-        }
+        }*/
 
         print("total rotation applied: " + rotationIncrement);
         print("total movement applied: " + positionIncrement);
 
-        transform.position += transform.right * positionIncrement * 0.1f;
-        transform.Translate(0,5,0);
+        
 
         //----testing----
-        print("testting start");
+        print("testing start");
         //Wait(30);
-        Say("hello", 2, false);
-        print("testting finish");
+        //Say("hello", 2, true);
+        print("testing finish");
         StartCoroutine(WaitUntil());
+        // transform.position += transform.right * positionIncrement * 0.1f;
+        // transform.Translate(0,5,0);
 
     }
 
@@ -52,28 +56,88 @@ public class Motion : MonoBehaviour
         {
             case "When": flagUsed = true; return;
             case "TL" : Turn("L", float.Parse(subStrings[1])); return; 
-            case "TR": Turn("R", float.Parse(subStrings[1])); return;
+            case "TR": Turn("R", -float.Parse(subStrings[1])); return;
             case "MV": Move(float.Parse(subStrings[1])); return;
+            case "repeat": Repeat(line, int.Parse(subStrings[1])); return;
+            case "CH": ChangePosition(subStrings[1], float.Parse(subStrings[2])); return;
+            case "SET": SetPosition(subStrings[1], float.Parse(subStrings[2])); return;
+            case "GO": SetPosition("X", float.Parse(subStrings[1])); SetPosition("Y", float.Parse(subStrings[2])); return;
         }
+
+    }
+
+    private void SetPosition(string variable, float value)
+    {
+        Vector3 newPos = Vector3.zero;
+        if (variable == "X")
+        {
+            newPos = new Vector3(value*factor, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            newPos = new Vector3(transform.position.x, value*factor, transform.position.z);
+        }
+        transform.position = newPos;
+    }
+
+    private void ChangePosition(string variable, float value)
+    {
+        if (variable == "X")
+        {
+            transform.Translate(value*factor, 0, 0);
+        }
+        else
+        {
+            transform.Translate(0, value*factor, 0);
+        }
+    }
+
+    private void Parser(string[] lines)
+    {
+        foreach (string line in lines)
+        {
+            print(line);
+            CheckKeyword(line);
+
+        }
+    }
+
+    private void Repeat(string line, int times)
+    {
+        // repeat 10 times #MV 10#
+        // repeatuntil yPos = 50 and xPos = 50 #SET X 0;CH Y 10#
+        string[] subStrings = line.Split('#');
+        String body = subStrings[1];
+        string[] lines = body.Split(';');
+
+        for (int i = 0; i < times; i++)
+        {
+            Parser(lines);
+        }
+
+
 
     }
 
     private void Turn(String direction, float value)
     {
-        if(direction == "L")
+        transform.Rotate(0, 0, value);
+        /*if(direction == "L")
         {
             // rotate left (positive rotation)
+            transform.Rotate(0, 0, value);
         }
         else
         {
             // rotate right (negative rotation)
-        }
+            transform.Rotate(0, 0, -value);
+        }*/
 
     }
 
     private void Move(float value)
     {
-        transform.position += transform.right * value * 0.1f;
+        transform.position += transform.right * value * factor;
     }
 
     // Update is called once per frame
@@ -98,10 +162,11 @@ public class Motion : MonoBehaviour
     }
     public void Say(string message,float time,Boolean isTime)
     {
+        // play meo music !!!
         say.text = message;
         if (isTime)
         {
-            InvokeRepeating("ClearSay", time, 0);  //1s delay, repeat every 1s
+            InvokeRepeating("ClearSay", time, 0);  //1s delay
         }
         
 
